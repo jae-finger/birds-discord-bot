d6dc06750bf28b251eef9af2bcdd5b6ba23a5453
Jonathan Finger <jaefinger@gmail.com>
2023-11-14 00:42:03 -0600

    Add Dockerfile and update Discord bot code.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # RescueTheBirds! Discord Bot
 Discord bot for our save-the-birds project
+
+
+`docker build -t discord-bot .`
+
+` docker run -e BIRB_DISCORD_TOKEN='XXXXXXXXXXX' discord-bot`
--- main.py
+++ main.py
@@ -7,20 +7,21 @@ from dotenv import load_dotenv
 load_dotenv()
 BIRB_RESCUE_TOKEN = os.getenv('BIRB_RESCUE_TOKEN')
 
-# Bot setup
-bot = commands.Bot(command_prefix="!")
-
-# Event example: Print a message when the bot is ready
-@bot.event
-async def on_ready():
-    print(f"{bot.user.name} has connected to Discord!")
-
-# Command example: Respond to the !hello command
-@bot.command(name='hello')
-async def hello(ctx):
-    await ctx.send("Hello!")
-
-# Run the bot
-bot.run(BIRB_RESCUE_TOKEN)
 intents = discord.Intents.default()
-intents.members = True
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(BIRB_RESCUE_TOKEN)
