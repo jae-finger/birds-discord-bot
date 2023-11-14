9e65a3da1fa696bdba8f4f9191c4ac5b611141a1
Jonathan Finger <jaefinger@gmail.com>
2023-11-14 00:53:48 -0600

    Fix environment variable name in main.py and update formatting in README.md

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Discord bot for our save-the-birds project
 
 `docker build -t discord-bot .`
 
-` docker run -e BIRB_DISCORD_TOKEN='XXXXXXXXXXX' discord-bot`
+`docker run -e BIRB_DISCORD_TOKEN='XXXXXXXXXXX' discord-bot`
--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 
 # ENV variables
 load_dotenv()
-BIRB_RESCUE_TOKEN = os.getenv('BIRB_RESCUE_TOKEN')
+
+BIRB_RESCUE_TOKEN = str(os.getenv('BIRB_DISCORD_TOKEN'))
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -24,4 +25,4 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run(BIRB_RESCUE_TOKEN)
+client.run(token=BIRB_RESCUE_TOKEN)
