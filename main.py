583ab72680ca2d2093d7b2b535998afb6f220750
Jonathan Finger <jaefinger@gmail.com>
2023-11-14 00:58:44 -0600

    Add command prefix and change hello message.

--- main.py
+++ main.py
@@ -11,18 +11,14 @@ BIRB_RESCUE_TOKEN = str(os.getenv('BIRB_DISCORD_TOKEN'))
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client = discord.Client(intents=intents, command_prefix='?')
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@client.command()
+async def hello(ctx):
+    await ctx.send('Ahoy!')
 
 client.run(token=BIRB_RESCUE_TOKEN)
