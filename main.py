69e5bdf63f735ac17e5bab252e983226cccbf993
Jonathan Finger <jaefinger@gmail.com>
2023-11-19 15:51:00 -0600

    Add event reminder cog and schedule event checking task

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import os
 from dotenv import load_dotenv
 import utils
@@ -33,7 +33,6 @@ intents.message_content = True
 intents.members = True
 intents.guilds = True
 
-
 # Create the bot
 bot = commands.Bot(
     command_prefix='?',
@@ -41,6 +40,11 @@ bot = commands.Bot(
     intents=intents,
     help_command=SimpleHelpCommand(no_category = 'Misc', sort_commands=True)
 )
+
+# load cogs
+async def setup():
+    await bot.load_extension('event_reminder_cog')
+
 
 # Events
 # On Ready Event
@@ -50,6 +54,7 @@ async def on_ready():
     print(f'We have logged in as {bot.user}')
     print(f'General Channel ID: {GENERAL_CHANNEL_ID}')
     print('--------------------------------')
+    event_check.start()
 
 @bot.event
 async def on_member_join(member):
@@ -74,6 +79,43 @@ async def source_code(ctx):
     hello_embed.set_image(url="https://raw.githubusercontent.com/jae-finger/birds-discord-bot/main/data/birb_logo_dalle.png")
     await ctx.send(embed=hello_embed)
 
+
+# Meetings
+async def check_next_event(bot):
+    guild = bot.get_guild(DISCORD_GUILD_ID)  # Replace with your guild ID
+    if guild:
+        next_event = await get_next_event(guild)  # Ensure this is an async function
+        if next_event:
+            time_until_event = next_event.start_time - datetime.now(timezone.utc)
+            if time_until_event <= timedelta(hours=1):
+                channel = bot.get_channel(GENERAL_CHANNEL_ID)
+                if channel:
+                    await channel.send(f"Reminder: '{next_event.name}' is happening in less than an hour!")
+                else:
+                    print(f"Unable to find channel with ID: {GENERAL_CHANNEL_ID}")
+            else:
+                print(f"Found an upcoming event: '{next_event.name}'. Will announce 1 hour before the event.")
+        else:
+            print("No upcoming events found at this time.")
+
+async def get_next_event(guild):
+    # Fetch the list of scheduled events for the guild
+    events = await guild.fetch_scheduled_events()
+
+    # Filter out events that have already started
+    future_events = [event for event in events if event.start_time > datetime.now(timezone.utc)]
+
+    # Sort them by start time
+    future_events.sort(key=lambda x: x.start_time)
+
+    # Return the next event or None if there are no future events
+    return future_events[0] if future_events else None
+
+
+@tasks.loop(minutes=60)
+async def event_check():
+    await check_next_event(bot)
+
 @bot.command()
 async def list_meetings(ctx):
     if ctx.message.author.id == DEFAULT_DISCORD_ID:
@@ -90,12 +132,6 @@ async def list_meetings(ctx):
         # Formatting the list of events
         event_list = '\n'.join([f'{event.name} - {event.start_time.strftime("%Y-%m-%d %H:%M")}' for event in events])
         await ctx.send(f'Scheduled Events:\n{event_list}')
-
-from datetime import datetime, timedelta
-import discord
-
-from datetime import datetime, timedelta
-import discord
 
 @bot.command()
 async def add_meeting(ctx, name: str, start_time_str: str, end_time_str: str, description: str = None, location: str = None):
@@ -131,6 +167,7 @@ async def add_meeting(ctx, name: str, start_time_str: str, end_time_str: str, de
             return
 
         # Create the scheduled event
+        image_url = "https://raw.githubusercontent.com/jae-finger/birds-discord-bot/main/data/birb_logo_dalle.png"
         event = await guild.create_scheduled_event(
             name=name, 
             start_time=start_time_cst, 
@@ -138,13 +175,10 @@ async def add_meeting(ctx, name: str, start_time_str: str, end_time_str: str, de
             description=description, 
             location=location, 
             entity_type=discord.EntityType.external,
-            privacy_level=discord.PrivacyLevel.guild_only
+            privacy_level=discord.PrivacyLevel.guild_only,
+            image=image_url
         )
         await ctx.send(f'Event "{name}" created successfully!')
-
-        # Create the scheduled event
-        # image_url = "https://raw.githubusercontent.com/jae-finger/birds-discord-bot/main/data/birb_logo_dalle.png"
-        # image=image_url
 
 @bot.command()
 async def announce_next_meeting(ctx):
@@ -180,9 +214,4 @@ async def announce_next_meeting(ctx):
     else:
         await ctx.send('General channel not found.')
 
-
-
-
-if __name__ == '__main__':
-    # Run the bot
-    bot.run(token=BIRB_RESCUE_TOKEN)
+bot.run(token=BIRB_RESCUE_TOKEN)
